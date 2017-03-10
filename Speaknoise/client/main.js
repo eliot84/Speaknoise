0dee8c96bf0c7c69cf28becec2631deece6e8127
Rodriguez <erodriguez@business.edu>
2017-03-10 13:59:39 -0500

    howler npm package now works. Added sound clips to public flder

--- Speaknoise/client/main.js
+++ Speaknoise/client/main.js
@@ -4,6 +4,7 @@ import { Meteor } from 'meteor/meteor';
 import howler from 'howler';
 
 
+Tracker.autorun
 console.log('b');
 var sound = new Howl({
   src: ['sound1.mp3', 'sound1.webm', 'sound1.wav']
