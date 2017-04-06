79f55969563c802ef370ddc41815540fff42141b
Rodriguez <erodriguez@business.edu>
2017-04-06 12:43:45 -0400

    Adding changes to the market page

--- Speaknoise/client/home/home.js
+++ Speaknoise/client/home/home.js
@@ -1,2 +1,11 @@
 import './home.html';
 import '/imports/style.css';
+
+
+Template.home.events({
+	'click [name=subscribeSubmit]': function(){
+		event.preventDefault();
+
+		console.log("HELLO!!");
+	}
+});
